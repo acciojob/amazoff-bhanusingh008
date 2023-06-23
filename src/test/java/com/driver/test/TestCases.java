815b5ace6b882998ab10a1aaa8da9085ea7f577e
bhanusingh008 <bhanusingh0605@gmail.com>
2023-06-24 04:49:14 +0530

    Test methods i guess were missing. So trying to add.

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -21,4 +21,29 @@ import java.util.List;
 @Ignore
 public class TestCases {
 
+    OrderController orderController = new OrderController();
+
+    public void testAddOrderPartnerPair(Order order){
+
+        orderController.addOrder(order);
+    }
+
+    public void testAddPartner(String partnerId){
+        orderController.addPartner(partnerId);
+    }
+
 }
+
+//TestCases.testAddOrder:25 » NullPointer
+//        Error:    TestCases.testAddOrderPartnerPair:53 » NullPointer
+//        Error:    TestCases.testAddPartner:34 » NullPointer
+//        Error:    TestCases.testDeletePartnerById:639 » NullPointer
+//        Error:    TestCases.testGetAllOrders:319 » NullPointer
+//        Error:    TestCases.testGetCountOfOrdersLeftAfterGivenTime:531 » NullPointer
+//        Error:    TestCases.testGetCountOfUnassignedOrders:389 » NullPointer
+//        Error:    TestCases.testGetLastDeliveryTime:593 » NullPointer
+//        Error:    TestCases.testGetOrderById:88 » NullPointer
+//        Error:    TestCases.testGetOrderCountByPartnerId:193 » NullPointer
+//        Error:    TestCases.testGetOrdersByPartnerId:239 » NullPointer
+//        Error:    TestCases.testGetPartnerById:142 » NullPointer
+//        Error:    TestCases.testOrderController:684 » NullPointer
